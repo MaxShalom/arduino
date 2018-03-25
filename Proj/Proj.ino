5c961aae6f9781fd90d642631ceacf698b190b41
Max Shalom <max_shalom121@flatbush.org>
2018-03-24 21:19:15 -0400

    For loops and LCD
    Added a preceding for loop for the buttonState to make sure the light is on and added code for an LCD screen.

--- Proj/Proj.ino
+++ Proj/Proj.ino
@@ -1,26 +1,47 @@
+  // Import
+  #include <LiquidCrystal.h>
+
   
   // Initialize 
-  const int rowOne = 13;       // the number of the LED pin
-  const int rowTwo =  12;      // the number of the LED pin
-  const int rowThree =  11;      // the number of the LED pin
-  const int rowFour =  10;      // the number of the LED pin
-  const int buttonOne =  13;      // the number of the Button pin
-  const int buttonTwo =  12;      // the number of the Button pin
-  const int buttonThree =  11;      // the number of the Button pin
-  const int buttonFour =  10;      // the number of the Button pin
-  const int row = random(10, 14);
-  const int buttonState = 0;  
+  const int rowOne = 13;           // the number of the LED pin
+  const int rowTwo =  12;
+  const int rowThree =  11;
+  const int rowFour =  10;
+  
+  const int buttonOne =  13;       // the number of the Button pin
+  const int buttonTwo =  12;
+  const int buttonThree =  11;
+  const int buttonFour =  10;
+  
+  const int row = random(10, 14);  //Chooses random light to light up
+
+  const int score;
+  
+  const int buttonStateOne = 0;    // Buttons
+  const int buttonStateTwo = 0;  
+  const int buttonStateThree = 0;  
+  const int buttonStateFour = 0;
+
+  const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+  LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(rowOne, OUTPUT);
   pinMode(rowTwo, OUTPUT);
   pinMode(rowThree, OUTPUT);
-  pinMode(ledFour, OUTPUT);
+  pinMode(rowFour, OUTPUT);
+  
   pinMode(buttonOne, INPUT);
   pinMode(buttonTwo, INPUT);
   pinMode(buttonThree, INPUT);
   pinMode(buttonFour, INPUT);
+
+  // set up the LCD's number of columns and rows:
+  lcd.begin(3, 1);
+  // Print a message to the LCD.
+  lcd.print(score);
 }
 
 void loop() {
@@ -32,23 +53,39 @@ void loop() {
   digitalWrite(row, HIGH);
   delay(2000);
   
-  if (buttonStateOne == HIGH) {
-    // turn LED on:
-    digitalWrite(buttonOne, Low);
+  if (rowOne = HIGH) {
+    if (buttonStateOne == HIGH) {
+      // turn LED off:
+      digitalWrite(buttonOne, LOW);
+      score++;
+      lcd.display();
+    }
   }
 
-  if (buttonStateTwo == HIGH) {
-    // turn LED on:
-    digitalWrite(buttonTwo, Low);
+  if (rowTwo = HIGH) {
+    if (buttonStateTwo == HIGH) {
+      // turn LED off:
+      digitalWrite(buttonTwo, LOW);
+      score++;
+      lcd.display();
+    }
   }
 
-  if (buttonStateThree == HIGH) {
-    // turn LED on:
-    digitalWrite(buttonThree, Low);
+  if (rowThree = HIGH) {
+    if (buttonStateThree == HIGH) {
+      // turn LED off:
+      digitalWrite(buttonThree, LOW);
+      score++;
+      lcd.display();
+    }
   }
 
-  if (buttonStateFour == HIGH) {
-    // turn LED on:
-    digitalWrite(buttonFour, Low);
+  if (rowFour = HIGH) {
+    if (buttonStateFour == HIGH) {
+      // turn LED off:
+      digitalWrite(buttonFour, LOW);
+      score++;
+      lcd.display();
+    }
   }
 }
