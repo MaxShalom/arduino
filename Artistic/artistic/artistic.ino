6b3bf2dab581e3fcdf3cce1439e2bed3a13e2c5a
Max Shalom <max_shalom121@flatbush.org>
2018-05-08 11:59:06 -0400

    Change Methods to shoot() instead of moveArm and moveLeg

--- Artistic/artistic/artistic.ino
+++ Artistic/artistic/artistic.ino
@@ -1,10 +1,9 @@
 #include <Servo.h>
 
-Servo armServo;  
+Servo armServo;
 Servo legServo;
 
 int pos = 0;
-int pos2 = 0;
 
 void setup() {
   armServo.attach(9);
@@ -12,29 +11,16 @@ void setup() {
 }
 
 void loop() {
-  moveArm();
-  moveLeg();
+  shoot();
 }
 
-void moveArm() {
-  for (pos = 0; pos <= 180; pos += 18) { 
-    armServo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(10);                       // waits 15ms for the servo to reach the position
+void shoot() {
+  for (pos = 0; pos <= 90; pos += 18) {
+    armServo.write(pos);
+    legServo.write(pos);
   }
-  for (pos = 180; pos >= 0; pos -= 18) { // goes from 180 degrees to 0 degrees
-    armServo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(10);                       // waits 15ms for the servo to reach the position
-  }
-}
-
-void moveLeg() {
-  for (pos2 = 0; pos2 <= 180; pos2 += 18) { 
-    legServo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(10);                       // waits 15ms for the servo to reach the position
-  }
-  for (pos2 = 180; pos2 >= 0; pos2 -= 18) { // goes from 180 degrees to 0 degrees
-    legServo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(10);                       // waits 15ms for the servo to reach the position
+  for (pos = 90; pos >= 0; pos -= 18) {
+    armServo.write(pos);
+    legServo.write(pos);
   }
 }
-
